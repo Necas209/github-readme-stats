9e3bc5af7990ad6199e5e7f79394c707c7b27fbc
Diogo Medeiros <diogo_medeiros@sapo.pt>
2024-06-18 01:36:26 +0100

    Formmatted files according to Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,78 +3,90 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
-import jsdoc from 'eslint-plugin-jsdoc';
+import jsdoc from "eslint-plugin-jsdoc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [...compat.extends("prettier"), {
+export default [
+  ...compat.extends("prettier"),
+  {
     languageOptions: {
-        globals: {
-            ...globals.node,
-            ...globals.browser,
-        },
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
 
-        ecmaVersion: 2022,
-        sourceType: "module",
+      ecmaVersion: 2022,
+      sourceType: "module",
     },
 
     plugins: {
-        "jsdoc": jsdoc,
+      jsdoc: jsdoc,
     },
 
     rules: {
-        "no-unexpected-multiline": "error",
+      "no-unexpected-multiline": "error",
 
-        "jsdoc/require-returns": "error",
-        "jsdoc/require-returns-description": "error",
-        "jsdoc/require-param-description": "error",
+      "jsdoc/require-returns": "error",
+      "jsdoc/require-returns-description": "error",
+      "jsdoc/require-param-description": "error",
 
-        "accessor-pairs": ["error", {
-            getWithoutSet: false,
-            setWithoutGet: true,
-        }],
+      "accessor-pairs": [
+        "error",
+        {
+          getWithoutSet: false,
+          setWithoutGet: true,
+        },
+      ],
 
-        "block-scoped-var": "warn",
-        "consistent-return": "error",
-        curly: "error",
-        "no-alert": "error",
-        "no-caller": "error",
+      "block-scoped-var": "warn",
+      "consistent-return": "error",
+      curly: "error",
+      "no-alert": "error",
+      "no-caller": "error",
 
-        "no-warning-comments": ["warn", {
-            terms: ["TODO", "FIXME"],
-            location: "start",
-        }],
+      "no-warning-comments": [
+        "warn",
+        {
+          terms: ["TODO", "FIXME"],
+          location: "start",
+        },
+      ],
 
-        "no-with": "warn",
-        radix: "warn",
-        "no-delete-var": "error",
-        "no-undef-init": "off",
-        "no-undef": "error",
-        "no-undefined": "off",
-        "no-unused-vars": "warn",
-        "no-use-before-define": "error",
-        "constructor-super": "error",
-        "no-class-assign": "error",
-        "no-const-assign": "error",
-        "no-dupe-class-members": "error",
-        "no-this-before-super": "error",
-        "object-shorthand": ["warn"],
-        "no-mixed-spaces-and-tabs": "warn",
-        "no-multiple-empty-lines": "warn",
-        "no-negated-condition": "warn",
-        "no-unneeded-ternary": "warn",
+      "no-with": "warn",
+      radix: "warn",
+      "no-delete-var": "error",
+      "no-undef-init": "off",
+      "no-undef": "error",
+      "no-undefined": "off",
+      "no-unused-vars": "warn",
+      "no-use-before-define": "error",
+      "constructor-super": "error",
+      "no-class-assign": "error",
+      "no-const-assign": "error",
+      "no-dupe-class-members": "error",
+      "no-this-before-super": "error",
+      "object-shorthand": ["warn"],
+      "no-mixed-spaces-and-tabs": "warn",
+      "no-multiple-empty-lines": "warn",
+      "no-negated-condition": "warn",
+      "no-unneeded-ternary": "warn",
 
-        "jsdoc/require-jsdoc": "warn",
+      "jsdoc/require-jsdoc": "warn",
 
-        "keyword-spacing": ["error", {
-            before: true,
-            after: true,
-        }],
+      "keyword-spacing": [
+        "error",
+        {
+          before: true,
+          after: true,
+        },
+      ],
     },
-}];
+  },
+];
